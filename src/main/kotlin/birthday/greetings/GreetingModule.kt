1ad37ff4ca0d9aa5998e4a16f9edafeb8309b987
Paolo Sciarra <itbhps@gmail.com>
2022-11-25 08:12:02 +0100

    [refactor] minimum refactor resource management

--- src/main/kotlin/birthday/greetings/GreetingModule.kt
+++ src/main/kotlin/birthday/greetings/GreetingModule.kt
@@ -1,6 +1,5 @@
 package birthday.greetings
 
-import java.io.File
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import javax.mail.Message
@@ -20,22 +19,23 @@ private val dateTimeFormatter =
   DateTimeFormatter.ofPattern("yyyy/MM/dd")
 
 fun sendGreetings(fileName: String, birthDate: String, smptHost: String, smtpPort: Int) {
-  File(object {}.javaClass.getResource("/$fileName")?.toURI())
-    .readLines()
-    .drop(1)
-    .map { csvLine ->
-      val (surname, name, dateAsString, email) = csvLine.split(", ")
-      Employee(name, surname, LocalDate.parse(dateAsString, dateTimeFormatter), email)
-    }
-    .filter { e -> e.dateOfBirth.isEqual(LocalDate.parse(birthDate, dateTimeFormatter)) }
-    .forEach {
-      System.setProperty("mail.smtp.host", smptHost)
-      System.setProperty("mail.smtp.port", smtpPort.toString())
-      val mimeMessage = MimeMessage(Session.getDefaultInstance(System.getProperties()))
-      mimeMessage.setFrom(InternetAddress("sender@email.com"))
-      mimeMessage.addRecipients(Message.RecipientType.TO, arrayOf(InternetAddress(it.email)))
-      mimeMessage.subject = "Happy Birthday!"
-      mimeMessage.setContent("Happy Birthday, dear ${it.name}!", "text/plain")
-      Transport.send(mimeMessage)
+  object {}.javaClass.getResourceAsStream("/$fileName")
+    ?.use {
+      it.reader(Charsets.UTF_8).readLines()
+        .drop(1)
+        .map { csvLine ->
+          val (surname, name, dateAsString, email) = csvLine.split(", ")
+          Employee(name, surname, LocalDate.parse(dateAsString, dateTimeFormatter), email)
+        }.filter { e -> e.dateOfBirth.isEqual(LocalDate.parse(birthDate, dateTimeFormatter)) }
+        .forEach {
+          System.setProperty("mail.smtp.host", smptHost)
+          System.setProperty("mail.smtp.port", smtpPort.toString())
+          val mimeMessage = MimeMessage(Session.getDefaultInstance(System.getProperties()))
+          mimeMessage.setFrom(InternetAddress("sender@email.com"))
+          mimeMessage.addRecipients(Message.RecipientType.TO, arrayOf(InternetAddress(it.email)))
+          mimeMessage.subject = "Happy Birthday!"
+          mimeMessage.setContent("Happy Birthday, dear ${it.name}!", "text/plain")
+          Transport.send(mimeMessage)
+        }
     }
 }
