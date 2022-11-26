b6341f3d28437022d961576d816ebdeb8c0d8ef6
Paolo Sciarra <itbhps@gmail.com>
2022-11-26 10:28:01 +0100

    [refactor] minor refactoring

--- src/main/kotlin/birthday/greetings/GreetingModule.kt
+++ src/main/kotlin/birthday/greetings/GreetingModule.kt
@@ -20,21 +20,25 @@ private val dateTimeFormatter =
 
 fun sendGreetings(fileName: String, birthDate: String, smptHost: String, smtpPort: Int) {
   object {}.javaClass.getResourceAsStream("/$fileName")
-    ?.use {
-      it.reader(Charsets.UTF_8).readLines()
+    ?.use { file ->
+      file.reader(Charsets.UTF_8)
+        .readLines()
         .drop(1)
         .map { csvLine ->
           val (surname, name, dateAsString, email) = csvLine.split(", ")
           Employee(name, surname, LocalDate.parse(dateAsString, dateTimeFormatter), email)
-        }.filter { e -> e.dateOfBirth.isEqual(LocalDate.parse(birthDate, dateTimeFormatter)) }
-        .forEach {
+        }
+        .filter { employee ->
+          employee.dateOfBirth.isEqual(LocalDate.parse(birthDate, dateTimeFormatter))
+        }
+        .forEach { employee ->
           System.setProperty("mail.smtp.host", smptHost)
           System.setProperty("mail.smtp.port", smtpPort.toString())
           val mimeMessage = MimeMessage(Session.getDefaultInstance(System.getProperties()))
           mimeMessage.setFrom(InternetAddress("sender@email.com"))
-          mimeMessage.addRecipients(Message.RecipientType.TO, arrayOf(InternetAddress(it.email)))
+          mimeMessage.addRecipients(Message.RecipientType.TO, arrayOf(InternetAddress(employee.email)))
           mimeMessage.subject = "Happy Birthday!"
-          mimeMessage.setContent("Happy Birthday, dear ${it.name}!", "text/plain")
+          mimeMessage.setContent("Happy Birthday, dear ${employee.name}!", "text/plain")
           Transport.send(mimeMessage)
         }
     }
